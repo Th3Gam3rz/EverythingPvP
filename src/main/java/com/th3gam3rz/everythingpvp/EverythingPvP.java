7502930fe9376756ba25508828cbf2f91b1051df
Th3Gam3rz <lcao1756@gmail.com>
2014-08-12 11:03:12 +0100

    Added /playerdrop command
    Fixed some bugs with saving blood color and playerdrop to config.

--- src/main/java/com/th3gam3rz/everythingpvp/EverythingPvP.java
+++ src/main/java/com/th3gam3rz/everythingpvp/EverythingPvP.java
@@ -30,16 +30,16 @@ public class EverythingPvP extends JavaPlugin {
 	}
 	
 	private boolean setupEconomy() {
-        	if (getServer().getPluginManager().getPlugin("Vault") == null) {
-            		return false;
-        	}
-        	RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
-        	if (rsp == null) {
-            		return false;
-        	}
-        	econ = rsp.getProvider();
-        	return econ != null;
-    	}
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
 
 	public boolean onCommand(CommandSender s, Command c, String l, String[] args) {
 		if(c.getName().equalsIgnoreCase("blood")){
@@ -55,6 +55,8 @@ public class EverythingPvP extends JavaPlugin {
 				if(args[0].equalsIgnoreCase("Red")){
 					try{
 						SettingsManager.getInstance().getSettings().set("BloodColor", "Red");
+						SettingsManager.getInstance().saveSettings();
+						SettingsManager.getInstance().reload();
 					}catch(Exception e){
 						e.printStackTrace();
 						ChatManager.getInstance().error("Unable to change the blood color. Try restarting the server!", s);
@@ -66,6 +68,8 @@ public class EverythingPvP extends JavaPlugin {
 				else if(args[0].equalsIgnoreCase("DarkRed")){
 					try{
 						SettingsManager.getInstance().getSettings().set("BloodColor", "DarkRed");
+						SettingsManager.getInstance().saveSettings();
+						SettingsManager.getInstance().reload();
 					}catch(Exception e){
 						e.printStackTrace();
 						ChatManager.getInstance().error("Unable to change the blood color. Try restarting the server!", s);
@@ -77,6 +81,8 @@ public class EverythingPvP extends JavaPlugin {
 				else if(args[0].equalsIgnoreCase("Blue")){
 					try{
 						SettingsManager.getInstance().getSettings().set("BloodColor", "Blue");
+						SettingsManager.getInstance().saveSettings();
+						SettingsManager.getInstance().reload();
 					}catch(Exception e){
 						e.printStackTrace();
 						ChatManager.getInstance().error("Unable to change the blood color. Try restarting the server!", s);
@@ -88,6 +94,8 @@ public class EverythingPvP extends JavaPlugin {
 				else if(args[0].equalsIgnoreCase("DarkBlue")){
 					try{
 						SettingsManager.getInstance().getSettings().set("BloodColor", "DarkBlue");
+						SettingsManager.getInstance().saveSettings();
+						SettingsManager.getInstance().reload();
 					}catch(Exception e){
 						e.printStackTrace();
 						ChatManager.getInstance().error("Unable to change the blood color. Try restarting the server!", s);
@@ -99,6 +107,8 @@ public class EverythingPvP extends JavaPlugin {
 				else if(args[0].equalsIgnoreCase("Green")){
 					try{
 						SettingsManager.getInstance().getSettings().set("BloodColor", "Green");
+						SettingsManager.getInstance().saveSettings();
+						SettingsManager.getInstance().reload();
 					}catch(Exception e){
 						e.printStackTrace();
 						ChatManager.getInstance().error("Unable to change the blood color. Try restarting the server!", s);
@@ -110,6 +120,8 @@ public class EverythingPvP extends JavaPlugin {
 				else if(args[0].equalsIgnoreCase("DarkGreen")){
 					try{
 						SettingsManager.getInstance().getSettings().set("BloodColor", "DarkGreen");
+						SettingsManager.getInstance().saveSettings();
+						SettingsManager.getInstance().reload();
 					}catch(Exception e){
 						e.printStackTrace();
 						ChatManager.getInstance().error("Unable to change the blood color. Try restarting the server!", s);
@@ -121,6 +133,8 @@ public class EverythingPvP extends JavaPlugin {
 				else if(args[0].equalsIgnoreCase("Multi")){
 					try{
 						SettingsManager.getInstance().getSettings().set("BloodColor", "Multi");
+						SettingsManager.getInstance().saveSettings();
+						SettingsManager.getInstance().reload();
 					}catch(Exception e){
 						e.printStackTrace();
 						ChatManager.getInstance().error("Unable to change the blood color. Try restarting the server!", s);
@@ -133,6 +147,37 @@ public class EverythingPvP extends JavaPlugin {
 					ChatManager.getInstance().error("Unknown color!", s);
 					return true;
 				}
+			}
+			ChatManager.getInstance().error("You do not have permission to do that!", s);
+			return true;
+		}
+		if(c.getName().equalsIgnoreCase("playerdrop")){
+			if(s.isOp() || s.hasPermission("pvp.playerdrop")){
+				if(args.length == 0){
+					ChatManager.getInstance().info("The current percentage dropped on death is " + SettingsManager.getInstance().getSettings().getInt("PercentageDroppedOnDeath") + ".", s);
+					ChatManager.getInstance().info("To change it, type /playerdrop <new value>. It must be a whole number between 0 and 100, inclusive.", s);
+					return true;
+				}
+				if(args.length > 1){
+					ChatManager.getInstance().error("Too many arguments!", s);
+					return true;
+				}
+				int arg;
+				try{
+					arg = Integer.parseInt(args[0]);
+				}catch(NumberFormatException e){
+					ChatManager.getInstance().error("The number you entered is not a whole number!", s);
+					return true;
+				}
+				if(arg < 0 || arg > 100){
+					ChatManager.getInstance().error("Error: The number you entered is less than 0 or greater than 100.", s);
+					return true;
+				}
+				SettingsManager.getInstance().getSettings().set("PercentageDroppedOnDeath", arg);
+				SettingsManager.getInstance().saveSettings();
+				SettingsManager.getInstance().reload();
+				ChatManager.getInstance().info("Set the percentage dropped on death to " + arg, s);
+				return true;
 			}
 			ChatManager.getInstance().error("You do not have permission to do that!", s);
 			return true;
