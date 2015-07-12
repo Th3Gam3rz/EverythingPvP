bf0e96356ca1e9a45b36506b847adc8f045003f2
Th3Gam3rz <lcao1756@gmail.com>
2015-07-12 11:12:31 +0100

    Create ChatManager.java

---
+++ src/main/java/com/featurededpic/everythingpvp/managers/ChatManager.java
@@ -0,0 +1,31 @@
+package com.featuredepic.everythingpvp.managers;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+public class ChatManager {
+	
+	private ChatManager() {
+	}
+	
+	private static ChatManager instance = new ChatManager();
+	
+	public static ChatManager getInstance(){
+		return instance;
+	}
+	
+	private static String PREFIX = ChatColor.GRAY + "[" + ChatColor.AQUA + "EverythingPvP" + ChatColor.GRAY + "] ";
+	
+	private static String getPrefix(){
+		return PREFIX;
+	}
+	
+	public void error(String msg, CommandSender sender){
+		sender.sendMessage(getPrefix() + ChatColor.RED + msg);
+	}
+	
+	public void info(String msg, CommandSender sender){
+		sender.sendMessage(getPrefix() + ChatColor.GREEN + msg);
+	}
+
+}
